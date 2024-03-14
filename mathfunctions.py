31d7947823ea8a355512075e3b67b0df564aff3a
Louisholmes0217 <louisholmes0217@gmail.com>
2024-03-14 00:20:33 +0000

    added slicing strings

---
+++ mathfunctions.py
@@ -0,0 +1,30 @@
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+# rounding a number, returns nearest full int
+print(round(pi))
+
+# rounding a number UP, returns upper int
+print(math.ceil(pi))
+
+# rounding a number DOWN, returns lower int
+print(math.floor(pi))
+
+# printing an absolute number, returns absolute value
+print(abs(-pi))
+
+# to the power of an exponent, returns final value
+print(pow(pi, 2))
+
+# square root, returns root
+print(math.sqrt(pi))
+
+# finds largest value within a list, returns largest number
+print(max(x, y, z))
+
+# finds smallest value within a list, returns smallest number
+print(min(x, y, z))
