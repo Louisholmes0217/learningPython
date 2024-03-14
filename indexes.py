ee74461afaf1701dd00ca2d39b7129d221f49659
Louisholmes0217 <louisholmes0217@gmail.com>
2024-03-14 01:38:02 +0000

    added indexes

---
+++ indexes.py
@@ -0,0 +1,19 @@
+# working with index operator
+
+name = "Awesome Name"
+
+# print first char of name
+print(name[0])
+
+# print last char of name
+print(name[-1])
+
+# print first 3 chars of name
+print(name[0:3])
+
+# print last 3 chars of name
+print(name[-3:])
+
+# print everything after the first 4 chars of name
+print(name[4:])
+
