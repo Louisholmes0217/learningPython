a5a79f87fd60ab073db5f2041f5502816626c96c
Louisholmes0217 <louisholmes0217@gmail.com>
2024-03-14 00:10:16 +0000

    added mathfunctions

---
+++ userinput.py
@@ -0,0 +1,11 @@
+# accepting user input
+
+# multiple step
+print("What is your name?")
+name = input()
+
+print("Hello", name)
+
+# Single step version
+name = input("What is you name?\n")
+print("Hello", name)
