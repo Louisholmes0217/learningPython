0a851ad221feda6570fcd23f6fde0f36ca4a1838
Louisholmes0217 <louisholmes0217@gmail.com>
2024-03-14 00:04:06 +0000

    Finished user input section

---
+++ typcasting.py
@@ -0,0 +1,19 @@
+# typecasting certain variables
+
+x = "1"
+y = 2.0
+z = 3
+
+# convert to int
+newx = int(x)
+
+# convert to string
+newy = str(y)
+
+# convert to float
+newz = float(z)
+
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
