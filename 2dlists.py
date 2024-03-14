7377b57050e83dfbb503fc6a45fae5cb27c52b9c
Louisholmes0217 <louisholmes0217@gmail.com>
2024-03-14 01:15:20 +0000

    added 2dlists

---
+++ 2dlists.py
@@ -0,0 +1,16 @@
+# 2d lists are simply lists of lists, and are indexed in the same way
+
+multiList = [
+            ["burger", "pizza", "cake"],
+            ["blue", "red", "green"],
+            ["big", "medium", "small"]
+            ]
+
+# printing entire 2d list
+print(multiList)
+
+# printing second list in the 2d list
+print(multiList[1])
+
+# printing second item from second list in 2d list
+print(multiList[1][1])
