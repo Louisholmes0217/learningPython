be7209c475f19962f0e8d30742830864ca2f3138
Louisholmes0217 <louisholmes0217@gmail.com>
2024-03-15 02:41:45 +0000

    added modules and messages

---
+++ modules.py
@@ -0,0 +1,9 @@
+# module is file containing python code, functions and variables
+import messages # importing module (we wrote this module)
+#import messages as msg # importing with alias
+#from messages import hello,bye  # directly importing functions
+# from messages import *    # this imports all functions and varibles directly
+
+messages.hello()    # calling functions
+messages.bye()
+
